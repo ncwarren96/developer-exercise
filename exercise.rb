cf4f2325b9997f371116173ef37d769ed8dac7da
Nick Warren <ncwarren@ucsc.edu>
2018-05-06 17:20:43 -0700

    Completed and tested even_fibonacci(nth) function

--- exercise.rb
+++ exercise.rb
@@ -21,14 +21,14 @@ class Exercise
   end
 
   # Takes in a string and returns the correct form of 'marklar' for that string.
-  def self.marklar_word(str)
+  def self.marklar_word(word)
     
     m = "marklar"
     
-    if str[0] === str[0].capitalize #check if string starts with capital letter
+    if word[0] === word[0].capitalize #check if string starts with capital letter
       m[0] = 'M'
-    elsif /[[:punct:]]$/.match?(str) #check if string ends with punctuation
-      m += str[str.length-1]
+    elsif /[[:punct:]]$/.match?(word) #check if string ends with punctuation
+      m += word[word.length-1]
     end 
     
     return m #return correctly formatted marklar
@@ -40,11 +40,26 @@ class Exercise
   # eg. the Fibonacci sequence up to 6 terms is (1, 1, 2, 3, 5, 8),
   # and the sum of its even numbers is (2 + 8) = 10
   def self.even_fibonacci(nth)
-    # TODO: Implement this method
+    
+    sum=0
+    for i in (0..nth)
+      f = fib(i)
+      if(f%2==0)
+        sum += f
+      end
+    end
+    return sum
   end
 
+  def self.fib(n)
+    if(n<2)
+      return n
+    else
+      return fib(n-1) + fib(n-2)
+    end
+  end
 end
 
+puts Exercise.even_fibonacci(10)
 
-str = Exercise.marklar(str)
 
